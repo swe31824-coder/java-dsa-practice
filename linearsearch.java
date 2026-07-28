6cfbf08d69315339d96e14a4a30867d8355338b2
swe31824-coder <swe31824@gmail.com>
2026-07-28 22:59:50 +0530

    Added Linear Search solution

---
+++ linearsearch.java
@@ -0,0 +1,14 @@
+public class linearsearch{
+    public static void main(String[]args){
+        int[]arr={1,4,6,7,8,9};
+        int n=arr.length;
+        int target=9;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target)
+            {
+                System.out.println("Index: " + i);
+                System.out.println("Value: " + arr[i]);
+            }
+        }
+    }
+}
