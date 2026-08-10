db3999b19a908a3de5739114c7a1bb6c72befa24
swe31824-coder <swe31824@gmail.com>
2026-08-10 06:40:58 +0530

    Added SelectionSort.java Solution

---
+++ SelectionSort.java
@@ -0,0 +1,18 @@
+public class SelectionSort{
+    public static void main(String[]args){
+        int[]arr={20,12,10,15,2};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+            System.out.println(arr[i]);
+        }
+    }
+}
