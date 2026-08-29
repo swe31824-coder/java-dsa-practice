6c88ea69e4e172f45c6e582a03033eef2839df1b
swe31824-coder <swe31824@gmail.com>
2026-08-29 20:21:18 +0530

    Added Demo.java Practice Solution

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+    public static void main(String[]args){
+        class Student{
+            int rollno;
+            float marks;
+            
+        } 
+        Student S=new Student();
+        System.out.println("S.rollno ="+723);
+        System.out.println("S.marks="+92.5);
+    
+    }
+}
