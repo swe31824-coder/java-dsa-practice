148a3259f8758a0b214170cccb2244e17a392c4f
swe31824-coder <swe31824@gmail.com>
2026-08-29 20:24:39 +0530

    Added Marks.java Solution

---
+++ Marks.java
@@ -0,0 +1,16 @@
+public class Marks {
+    public static void main(String[]args){
+        int marks=70;
+        if(marks>=90){
+            System.out.println("First grade="+marks);
+        }
+        else if(marks>=80){
+            System.out.println("Second grade="+marks);
+        }
+        else if(marks>=70){
+            System.out.println("Third grade="+marks);
+        }
+        else
+            System.out.println("fail="+marks);
+    }
+}
